679230f75b9e9f6015517442ce4121d0fd9653dd
korAPUcard <tmdrnr1157@gmail.com>
2023-09-01 06:17:00 +0900

    Update shared_form_func.js

--- shared_form_func.js
+++ shared_form_func.js
@@ -1,3 +1,11 @@
+fetch("/shared_form_style.css").then(response => {
+    if (response.ok) {
+        response.text().then(text => {
+            document.head.appendChild(document.createTextNode(text));
+        });
+    }
+});
+
 document.body.style.overflowX = "hidden";
 document.body.style.overflowY = "auto";
 
